a0cbc09f9c7d748f8489b78f4c9159686fc1539f
hunterD3CD <hunterd3c@gmail.com>
2021-06-20 22:11:14 -0700

    weather api

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
-fetch(
-  `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=c8050763e934a3e49035af066c6fde69`
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    console.log(response);
-  });
+// ------------------------------------ESTABLISHING VARIABLES-----------------------------------
+// variable: city
+var cities = [];
+var cityInputE1 = $("#city");
+// // variable: function
+// var submit = function (event) {
+//   event.preventDefault();
+//   var city = cityInputEl.value.trim();
+//   if (city) {
+//     getCityWeather(city);
+//     get5Day(city);
+//     cities.unshift({ city });
+//     cityInputEl.value = "";
+//   } else {
+//     alert("Please enter a City");
+//   }
+//   saveSearch();
+//   pastSearch(city);
+// };
+
+// var saveSearch = function () {
+//   localStorage.setItem("cities", JSON.stringify(cities));
+// };
+// var getWeather = function (city) {
+//   var apiKey = "c8050763e934a3e49035af066c6fde69";
+//   fetch(
+//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+//   )
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (response) {
+//       console.log(response);
+//     });
+// };
+// ------------------------------------------WEATHER API-------------------------------------
+// When search button is clicked, read the city name and get the weather response
+var apiFunction = function (event) {
+  event.preventDefault();
+  var city = document.querySelector("#city").value;
+  var apiKey = "c8050763e934a3e49035af066c6fde69";
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+};
+
+document.getElementById("searchBtn").onclick = apiFunction;
+
+// When search button is clicked, city name is stored in the local storage
+var saveSearch = function (event) {
+  event.preventDefault();
+  var city = document.querySelector("#city").value;
+  localStorage.setItem("city", city);
+};
+
+document.getElementById("searchBtn").onclick = saveSearch;
